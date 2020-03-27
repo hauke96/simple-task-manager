afda74c5e7eb2c8f988ca4e30fcf0556f7d60ec0
Hauke Stieler <mail@hauke-stieler.de>
2020-03-27 15:24:30 +0100

    Add project material and adjust style

--- client/src/app/project/project.service.ts
+++ client/src/app/project/project.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Project } from './project.material';
 
 @Injectable({
   providedIn: 'root'
@@ -7,12 +8,12 @@ export class ProjectService {
   public projects: string[] = [];
 
   constructor() {
-    this.projects[0] = 'Test';
-    this.projects[1] = 'foo';
-    this.projects[2] = 'bar';
+    this.projects[0] = new Project('1', 'Test');
+    this.projects[1] = new Project('2', 'foo');
+    this.projects[2] = new Project('3', 'bar');
   }
 
-  public getProjects() {
+  public getProjects() : Project[] {
     return this.projects;
   }
 }
