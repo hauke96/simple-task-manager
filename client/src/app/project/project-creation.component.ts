8d87a078f83d9f1552fd37e613988c5c74b58c7c
Hauke Stieler <mail@hauke-stieler.de>
2020-03-28 17:28:49 +0100

    Draw geometries on map and turn them into tasks

--- client/src/app/project/project-creation.component.ts
+++ client/src/app/project/project-creation.component.ts
@@ -1,22 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProjectService } from './project.service';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import { OSM } from 'ol/source';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import { defaults as defaultControls, ScaleLine, Attribution } from 'ol/control';
+import { Polygon } from 'ol/geom';
+import { Projection } from 'ol/proj';
+import { Style, Stroke, Fill } from 'ol/style';
+import { Feature } from 'ol';
+import { Draw } from 'ol/interaction';
 
 @Component({
   selector: 'app-project-creation',
   templateUrl: './project-creation.component.html',
   styleUrls: ['./project-creation.component.scss']
 })
-export class ProjectCreationComponent implements OnInit {
+export class ProjectCreationComponent implements OnInit, AfterViewInit {
   public newProjectName: string;
+
+  private map: Map;
+  private vectorSource: VectorSource;
 
   constructor(private projectService: ProjectService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    // Simple style function the the polygons
+    const style = (feature, resolution) => {
+      let borderColor = '#26a69a90';
+      let fillColor = '#80cbc430';
+
+      return new Style({
+        stroke: new Stroke({
+          color: borderColor,
+          width: 2,
+        }),
+        fill: new Fill({
+          color: fillColor
+        })
+      })
+    };
+    
+    // this vector source contains all the task geometries
+    this.vectorSource = new VectorSource();
+    const vectorLayer = new VectorLayer({
+      source: this.vectorSource,
+      style: style
+    });
+
+    this.map = new Map({
+      target: 'map',
+      controls: defaultControls().extend([
+        new ScaleLine(),
+        new Attribution()
+      ]),
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        }),
+        vectorLayer
+      ],
+      view: new View({
+        center: [1110161, 7085688],
+        projection: 'EPSG:3857',
+        zoom: 14,
+        minZoom: 0,
+        maxZoom: 19
+      })
+    });
+
+    const draw = new Draw({
+      source: this.vectorSource,
+      type: 'Polygon'
+    });
+    this.map.addInteraction(draw);
+   }
+
   public onSaveButtonClicked() {
-    this.projectService.createNewProject(this.newProjectName);
+    const coordinates: [[number, number]][] = [];
+    this.vectorSource.getFeatures().map(f => {
+      let polygon = (f.getGeometry() as Polygon);
+      polygon = polygon.transform('EPSG:3857', 'EPSG:4326');
+      // The openlayers "Polygon" Class can contain multiple rings. Because the
+      // user just draws things, there only exist polygons having only one ring.
+      // Therefore we take the first and only ring as our task geometry.
+      coordinates.push(polygon.getCoordinates()[0]);
+    });
+
+    this.projectService.createNewProject(this.newProjectName, coordinates);
     this.router.navigate(['/manager']);
   }
 }
--- client/src/app/project/project.service.ts
+++ client/src/app/project/project.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Project } from './project.material';
+import { Task } from './../task/task.material';
+import { TaskService } from './../task/task.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +9,7 @@ import { Project } from './project.material';
 export class ProjectService {
   public projects: Project[] = [];
 
-  constructor() {
+  constructor(private taskService: TaskService) {
     this.projects[0] = new Project('p1', 'Test', ['t0', 't1']);
     this.projects[1] = new Project('p2', 'foo', ['t2']);
     this.projects[2] = new Project('p3', 'bar', ['t3', 't4']);
@@ -21,8 +23,12 @@ export class ProjectService {
     return this.projects.find(p => p.id == id);
   }
 
-  public createNewProject(name: string) {
-    const p = new Project('p-'+Math.random().toString(36).substring(7), name, []);
+  public createNewProject(name: string, geometries: [[number, number]][]) {
+    // Create new tasks with the given geometries and collect their IDs
+    const tasks = geometries.map(g => this.taskService.createNewTask(g));
+    console.log(tasks);
+
+    const p = new Project('p-'+Math.random().toString(36).substring(7), name, tasks);
     this.projects.push(p);
   }
 }
