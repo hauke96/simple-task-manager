41dcd38a115afa73c0f02cf7070d92e84ed7851c
Hauke Stieler <mail@hauke-stieler.de>
2020-03-28 13:57:15 +0100

    Set user name correctly on the UserService

---
+++ client/src/app/auth/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private currentUser?: string;
+
+  // TODO changed event?
+
+  constructor() { }
+
+  public setUser(user: string) {
+    this.currentUser = user;
+  }
+
+  public resetUser() {
+    this.currentUser = undefined;
+  }
+
+  public getUser(): string {
+    return this.currentUser;
+  }
+}
