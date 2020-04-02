139b44d09bb7caf9a68b49f7baf4d45686eac4cb
Hauke Stieler <mail@hauke-stieler.de>
2020-04-02 23:57:36 +0200

    Set version to 0.3.1

--- server/main.go
+++ server/main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/hauke96/sigolo"
 )
 
-const VERSION string = "0.3.1-dev"
+const VERSION string = "0.3.1"
 
 var (
 	app       = kingpin.New("Simple Task Manager", "A tool dividing an area of the map into smaller tasks.")
