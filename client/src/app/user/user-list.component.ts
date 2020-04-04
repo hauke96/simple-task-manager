f689553a6f12a7e7ac71b429b3da3e23748d0344
Hauke Stieler <mail@hauke-stieler.de>
2020-04-04 17:50:02 +0200

    Show list of users

--- client/src/app/user/user-list.component.ts
+++ client/src/app/user/user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
+  @Input() users: string[];
 
-  constructor() { }
+  constructor( ) { }
 
   ngOnInit(): void {
   }
-
 }
