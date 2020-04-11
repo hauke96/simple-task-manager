84f21bfcc5b2ac803e038139cb1777f38be3b72e
Hauke Stieler <mail@hauke-stieler.de>
2020-04-11 12:45:14 +0200

    Add test for project-settings component

--- client/src/app/project/project-settings.component.spec.ts
+++ client/src/app/project/project-settings.component.spec.ts
@@ -3,10 +3,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProjectSettingsComponent } from './project-settings.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ProjectService } from './project.service';
+import { of, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 describe('ProjectSettingsComponent', () => {
   let component: ProjectSettingsComponent;
   let fixture: ComponentFixture<ProjectSettingsComponent>;
+  let projectService: ProjectService;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,9 +19,16 @@ describe('ProjectSettingsComponent', () => {
       imports: [
         HttpClientTestingModule,
         RouterTestingModule.withRoutes([])
+      ],
+      providers: [
+        ProjectService
       ]
     })
     .compileComponents();
+
+    projectService = TestBed.get(ProjectService);
+
+    router = TestBed.get(Router);
   }));
 
   beforeEach(() => {
@@ -27,5 +39,31 @@ describe('ProjectSettingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should make service call', () => {
+    spyOn(projectService, 'deleteProject').and.callFake((id: string) => {
+      expect(id).toEqual('1');
+      return of({});
+    });
+    spyOn(router, 'navigate').and.callFake((commands: any[]) => {
+      expect(commands[0]).toEqual('/manager');
+      return of(true).toPromise();
+    });
+    component.projectId = '1';
+
+    component.onDeleteButtonClicked();
+  });
+
+  it('should not navigate on error', () => {
+    spyOn(projectService, 'deleteProject').and.callFake((id: string) => {
+      expect(id).toEqual('1');
+      return throwError('Test-error');
+    });
+    component.projectId = '1';
+
+    component.onDeleteButtonClicked();
+
+    expect(spyOn(router, 'navigate')).not.toHaveBeenCalled();
   });
 });
