c6be73b781ab807f26452d8f3f83130826740193
Hauke Stieler <mail@hauke-stieler.de>
2020-04-18 14:16:11 +0200

    Fix test

--- client/src/app/project/project-list.component.spec.ts
+++ client/src/app/project/project-list.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProjectListComponent } from './project-list.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from '../user/user.service';
 
 describe('ProjectListComponent', () => {
   let component: ProjectListComponent;
@@ -14,6 +15,9 @@ describe('ProjectListComponent', () => {
       imports: [
         HttpClientTestingModule,
         RouterTestingModule.withRoutes([])
+      ],
+      providers: [
+        UserService
       ]
     })
       .compileComponents();
