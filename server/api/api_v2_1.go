0b7272561d23daed293f6d4862c83a54185392b0
Hauke Stieler <mail@hauke-stieler.de>
2020-04-19 12:48:49 +0200

    Add API v2.1

---
+++ server/api/api_v2_1.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+
+	"../auth"
+	"../project"
+	"../util"
+)
+
+func Init_v2_1(router *mux.Router) (*mux.Router, string) {
+	r := router.PathPrefix("/v2").Subrouter()
+
+	r.HandleFunc("/projects/{id}", authenticatedHandler(getProject_v2_1)).Methods(http.MethodGet)
+
+	// Old from v2
+	r.HandleFunc("/projects/{id}", authenticatedHandler(deleteProjects_v2)).Methods(http.MethodDelete)
+	r.HandleFunc("/projects/{id}/tasks", authenticatedHandler(getProjectTasks_v2)).Methods(http.MethodGet)
+	r.HandleFunc("/projects/{id}/users", authenticatedHandler(leaveProject_v2)).Methods(http.MethodDelete)
+	r.HandleFunc("/projects/{id}/users", authenticatedHandler(addUserToProject_v2)).Methods(http.MethodPost)
+
+	r.HandleFunc("/tasks/{id}/assignedUser", authenticatedHandler(assignUser_v2)).Methods(http.MethodPost)
+	r.HandleFunc("/tasks/{id}/assignedUser", authenticatedHandler(unassignUser_v2)).Methods(http.MethodDelete)
+	r.HandleFunc("/tasks/{id}/processPoints", authenticatedHandler(setProcessPoints_v2)).Methods(http.MethodPost)
+
+	// Same as in v1:
+	r.HandleFunc("/projects", authenticatedHandler(getProjects)).Methods(http.MethodGet)
+	r.HandleFunc("/projects", authenticatedHandler(addProject)).Methods(http.MethodPost)
+	r.HandleFunc("/tasks", authenticatedHandler(addTask)).Methods(http.MethodPost)
+
+	return r, "v2.1"
+}
+
+func getProject_v2_1(w http.ResponseWriter, r *http.Request, token *auth.Token) {
+	vars := mux.Vars(r)
+
+	tasks, err := project.GetProject(vars["id"])
+	if err != nil {
+		util.ResponseInternalError(w, err.Error())
+		return
+	}
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(tasks)
+}
