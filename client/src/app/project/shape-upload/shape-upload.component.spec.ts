919f8832b17e3e076625cde628f0b94367ca29ee
Hauke Stieler <mail@hauke-stieler.de>
2020-04-29 16:59:02 +0200

    Add GPX file upload

--- client/src/app/project/shape-upload/shape-upload.component.spec.ts
+++ client/src/app/project/shape-upload/shape-upload.component.spec.ts
@@ -1,16 +1,63 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShapeUploadComponent } from './shape-upload.component';
+import { ErrorService } from '../../common/error.service';
+
+const exampleGpxFile = `
+<gpx>
+  <wpt lat="53.55425510342779" lon="9.945566643476486">
+  </wpt>
+  <trk>
+    <trkseg>
+      <trkpt lat="53.559532123118686" lon="9.942970232963562">
+      </trkpt>
+      <trkpt lat="53.55713588559855" lon="9.943098978996275">
+      </trkpt>
+      <trkpt lat="53.55718687078871" lon="9.946489291191103">
+      </trkpt>
+      <trkpt lat="53.559481140754805" lon="9.94631762981415">
+      </trkpt>
+      <trkpt lat="53.559532123118686" lon="9.942970232963562">
+      </trkpt>
+    </trkseg>
+    <trkseg>
+      <trkpt lat="53.55711039298042" lon="9.949493365287783">
+      </trkpt>
+      <trkpt lat="53.559608596549324" lon="9.950737910270691">
+      </trkpt>
+      <trkpt lat="53.55698292965938" lon="9.952583270072939">
+      </trkpt>
+      <trkpt lat="53.55711039298042" lon="9.949493365287783">
+      </trkpt>
+    </trkseg>
+  </trk>
+  <rte>
+      <rtept lat="53.55945564954981" lon="9.95498652935028">
+      </rtept>
+      <rtept lat="53.557084900346936" lon="9.957175211906435">
+      </rtept>
+      <rtept lat="53.55726334845877" lon="9.960865931510925">
+      </rtept>
+      <rtept lat="53.55981252502185" lon="9.961295084953308">
+      </rtept>
+      <rtept lat="53.55967230518426" lon="9.959439028501508">
+      </rtept>
+  </rte>
+</gpx>
+`;
 
 describe('ShapeUploadComponent', () => {
   let component: ShapeUploadComponent;
   let fixture: ComponentFixture<ShapeUploadComponent>;
+  let errorService: ErrorService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ShapeUploadComponent ]
+      declarations: [ShapeUploadComponent]
     })
-    .compileComponents();
+      .compileComponents();
+
+    errorService = TestBed.inject(ErrorService);
   }));
 
   beforeEach(() => {
@@ -21,5 +68,15 @@ describe('ShapeUploadComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should read GPX file', () => {
+    const features = component.fileToFeatures('example.gpx', exampleGpxFile);
+
+    expect(features.length).toEqual(3);
+  });
+
+  it('should fail on unknown file extension', () => {
+    expect(() => component.fileToFeatures('example.foo', '')).toThrow();
   });
 });
--- client/src/app/project/shape-upload/shape-upload.component.ts
+++ client/src/app/project/shape-upload/shape-upload.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ErrorService } from '../../common/error.service';
+import { Feature } from 'ol';
+import { GPX } from 'ol/format';
+import GeometryType from 'ol/geom/GeometryType';
+import { LineString, MultiLineString, Polygon } from 'ol/geom';
 
 @Component({
   selector: 'app-shape-upload',
@@ -7,6 +11,7 @@ import { ErrorService } from '../../common/error.service';
   styleUrls: ['./shape-upload.component.scss']
 })
 export class ShapeUploadComponent implements OnInit {
+  @Output() public featuresUploaded = new EventEmitter<Feature[]>();
 
   constructor(
     private errorService: ErrorService
@@ -18,15 +23,64 @@ export class ShapeUploadComponent implements OnInit {
 
   public onFileSelected(event: any) {
     const reader = new FileReader();
-    reader.readAsText(event.target.files[0], 'UTF-8');
+    const file = event.target.files[0];
+
+    reader.readAsText(file, 'UTF-8');
 
     reader.onload = (evt) => {
-      // TODO use service to turn file into polygons
-      console.log(evt.target.result);
+      try {
+        const features = this.fileToFeatures(file.name, evt.target.result);
+        this.featuresUploaded.emit(features);
+      } catch (e) {
+        this.errorService.addError(e);
+      }
     };
     reader.onerror = (evt) => {
       console.error(evt);
       this.errorService.addError('Could not upload file \'${evt.target.files[0]}\'');
     };
+  }
+
+  public fileToFeatures(fileName: any, content: string | ArrayBuffer): Feature[] {
+    let features: Feature[];
+
+    if (fileName.endsWith('.gpx')) {
+      features = this.gpxToFeatures(content);
+    } else {
+      throw new Error(`Unknown file type of file ${fileName}`);
+    }
+
+    return features;
+  }
+
+  public gpxToFeatures(result: string | ArrayBuffer): Feature[] {
+    const features: Feature[] = [];
+    const fmt = new GPX();
+
+    const rawFeatures = fmt.readFeatures(result);
+
+    // Turn (multi) line strings into separate polygons
+    rawFeatures.forEach(rawFeature => {
+      let lineStrings: LineString[];
+
+      // A GPX file can contain both, line string and multi line strings
+      const geometryType = rawFeature.getGeometry().getType();
+      if (geometryType === GeometryType.MULTI_LINE_STRING) {
+        const multiLineString = (rawFeature.getGeometry() as MultiLineString);
+        lineStrings = multiLineString.getLineStrings();
+      } else if (geometryType === GeometryType.LINE_STRING) {
+        lineStrings = [(rawFeature.getGeometry() as LineString)];
+      } else if (geometryType === GeometryType.POINT) {
+        return;
+      } else {
+        throw new Error(`Invalid geometry type ${geometryType} in GPX file`);
+      }
+
+      const polygons = lineStrings.map(s => new Polygon([s.getCoordinates()]));
+      const actualFeatures = polygons.map(p => new Feature(p));
+      actualFeatures.forEach(f => features.push(f));
+    });
+
+    return features;
   }
 }
