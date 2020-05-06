203a561e48020e5e50c8bdf5d85e3549d6ab6642
Hauke Stieler <mail@hauke-stieler.de>
2020-05-06 19:21:07 +0200

    Only return errors from permission service

--- server/permission/permission.go
+++ server/permission/permission.go
@@ -21,46 +21,58 @@ func Init() {
 }
 
 // VerifyOwnership check if the given user is the owner of the given project.
-func VerifyOwnership(projectId string, user string) (bool, error) {
+func VerifyOwnership(projectId string, user string) error {
 	query := fmt.Sprintf("SELECT * FROM %s WHERE projectId=$1 AND owner=$2", projectTable)
 
 	util.LogQuery(query, projectId, user)
 	rows, err := db.Query(query, projectId, user)
 	if err != nil {
-		return false, errors.Wrap(err, fmt.Sprintf("error verifying ownership of user %s in project %s", user, projectId))
+		return errors.Wrap(err, fmt.Sprintf("error verifying ownership of user %s in project %s", user, projectId))
 	}
 	defer rows.Close()
 
 	// If there's a next row, then the user "user" is in the owner of the project "projectId"
-	return rows.Next(), nil
+	if !rows.Next() {
+		return errors.New(fmt.Sprintf("user %s is not the owner of project %s", user, projectId)
+	}
+
+	return nil
 }
 
 // VerifyMembershipProject checks if "user" is a member of the project "id".
-func VerifyMembershipProject(projectId string, user string) (bool, error) {
+func VerifyMembershipProject(projectId string, user string) error {
 	query := fmt.Sprintf("SELECT * FROM %s WHERE projectId=$1 AND $2=ANY(users)", projectTable)
 
 	util.LogQuery(query, projectId, user)
 	rows, err := db.Query(query, projectId, user)
 	if err != nil {
-		return false, errors.Wrap(err, fmt.Sprintf("error verifying membership of user %s in project %s", user, projectId))
+		return errors.Wrap(err, fmt.Sprintf("error verifying membership of user %s in project %s", user, projectId))
 	}
 	defer rows.Close()
 
 	// If there's a next row, then the user "user" is in the list of members of project "projectId"
-	return rows.Next(), nil
+	if !rows.Next() {
+		return errors.New(fmt.Sprintf("user %s is not a member of project %s", user, projectId)
+	}
+
+	return nil
 }
 
 // VerifyMembershipTask checks if "user" is a member of the project, where the given task with "id" is in.
-func VerifyMembershipTask(taskId string, user string) (bool, error) {
+func VerifyMembershipTask(taskId string, user string) error {
 	query := fmt.Sprintf("SELECT * FROM %s WHERE $1=ANY(task_ids) AND $2=ANY(users);", projectTable)
 
 	util.LogQuery(query, taskId, user)
 	rows, err := db.Query(query, taskId, user)
 	if err != nil {
-		return false, errors.Wrap(err, fmt.Sprintf("error verifying membership of user %s in task %s", user, taskId))
+		return errors.Wrap(err, fmt.Sprintf("error verifying membership of user %s in task %s", user, taskId))
 	}
 	defer rows.Close()
 
 	// If there's a next row, then the given task in in the list of a project where the given user is a member of.
-	return rows.Next(), nil
+	if !rows.Next() {
+		return errors.New(fmt.Sprintf("user %s is not a member of the project where the task %s is in", user, taskId)
+	}
+
+	return nil
 }
