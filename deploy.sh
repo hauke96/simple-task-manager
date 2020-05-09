18ec4fbbd8a46843ad4aa48cf93ddc7bf5526a24
Hauke Stieler <mail@hauke-stieler.de>
2020-05-09 18:04:31 +0200

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 # Some logging to see how long building an deployment takes plus the deployment itself.
 
 echo -n "OAuth consumer key: "
-read $OAUTH_CONSUMER_KEY
+read OAUTH_CONSUMER_KEY_INPUT
 
 echo -n "OAuth secret: "
-read -s $OAUTH_SECRET
+read -s OAUTH_SECRET_INPUT
 echo
 
-export OAUTH_CONSUMER_KEY=$OAUTH_CONSUMER_KEY
-export OAUTH_SECRET=$OAUTH_SECRET
+export OAUTH_CONSUMER_KEY=$OAUTH_CONSUMER_KEY_INPUT
+export OAUTH_SECRET=$OAUTH_SECRET_INPUT
 
 clear
 date
