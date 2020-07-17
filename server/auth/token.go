134f3b8320720a7f9b83f606666af3a75fd3aa98
Hauke Stieler <mail@hauke-stieler.de>
2020-07-17 15:12:14 +0200

    Remove test code

--- server/auth/token.go
+++ server/auth/token.go
@@ -24,9 +24,9 @@ var (
 )
 
 func tokenInit() error {
-	bytes := []byte("test")//getRandomBytes(256)
+	bytes, err := getRandomBytes(256)
 	key = bytes
-	return nil
+	return err
 }
 
 func createTokenString(err error, userName string, userId string, validUntil int64) (string, error) {
