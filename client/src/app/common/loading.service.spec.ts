0da6822a44340468d4a7ee4ea8943ad0d95fdcad
Hauke Stieler <mail@hauke-stieler.de>
2020-08-30 18:48:06 +0200

    Add tests to loading service

--- client/src/app/common/loading.service.spec.ts
+++ client/src/app/common/loading.service.spec.ts
@@ -1,15 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoadingService } from './loading.service';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ReplaySubject } from 'rxjs';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent } from '@angular/router';
 
 describe('LoadingService', () => {
   let service: LoadingService;
 
+  const routerEventSubject = new ReplaySubject<RouterEvent>();
+  const routerMock = {
+    navigate: jasmine.createSpy('navigate'),
+    events: routerEventSubject.asObservable(),
+    url: '/foo/bar'
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule.withRoutes([])
+      providers: [
+        {
+          provider: Router,
+          useValue: routerMock
+        }
       ]
     });
     service = TestBed.inject(LoadingService);
@@ -17,14 +28,40 @@ describe('LoadingService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-    expect(service.isLoading).toBeFalse();
+    expect(service.isLoading()).toBeFalse();
   });
 
   it('should set loading state correctly', () => {
     service.start();
-    expect(service.isLoading).toBeTrue();
+    expect(service.isLoading()).toBeTrue();
 
     service.end();
-    expect(service.isLoading).toBeFalse();
+    expect(service.isLoading()).toBeFalse();
+  });
+
+  it('should start loading on routing start event', () => {
+    routerEventSubject.next(new NavigationStart(1, '/foo/bar'));
+    expect(service.isLoading()).toBeTrue();
+  });
+
+  describe('navigation end events', () => {
+    beforeEach(() => {
+      service.start();
+    });
+
+    it('should end loading on navigation error', () => {
+      routerEventSubject.next(new NavigationError(1, '/foo/bar', 'some error'));
+      expect(service.isLoading()).toBeFalse();
+    });
+
+    it('should end loading on navigation cancellation', () => {
+      routerEventSubject.next(new NavigationCancel(1, '/foo/bar', 'some error'));
+      expect(service.isLoading()).toBeFalse();
+    });
+
+    it('should end loading on navigation end', () => {
+      routerEventSubject.next(new NavigationEnd(1, '/foo/bar', '/some/other/url'));
+      expect(service.isLoading()).toBeFalse();
+    });
   });
 });
