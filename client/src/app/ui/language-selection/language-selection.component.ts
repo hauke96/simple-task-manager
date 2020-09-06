3cbf0d1d55feebb806b075c59bdca70f050299bc
Hauke Stieler <mail@hauke-stieler.de>
2020-09-06 14:17:27 +0200

    Set default selection and navigate to selected lang

--- client/src/app/ui/language-selection/language-selection.component.ts
+++ client/src/app/ui/language-selection/language-selection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 export class Language {
   constructor(public code: string, public name: string) {
@@ -12,20 +13,28 @@ export class Language {
 })
 export class LanguageSelectionComponent implements OnInit {
   languages = [
-    new Language('en', 'English'),
+    new Language('en-US', 'English'),
     new Language('de', 'Deutsch'),
     new Language('ja', '日本人'),
-    new Language('zh-cn', '中文'),
+    new Language('zh-CN', '中文'),
   ];
   selectedLanguage: Language;
 
-  constructor() {
+  constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+    const url = this.route.snapshot.url;
+    const allLangCodes = this.languages.map(l => l.code);
+
+    if (!url || url.length === 0 || !allLangCodes.includes(url[0].path)) {
+      this.selectedLanguage = this.languages[0]; // en-US as default
+    } else {
+      this.selectedLanguage = this.languages.find(l => l.code === url[0].path);
+    }
   }
 
   onLanguageChange() {
-    console.log(this.selectedLanguage);
+    location.href = this.selectedLanguage.code + '/manager';
   }
 }
