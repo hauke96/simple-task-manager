419b3b235f095321f5efc8ecf7a36a7e0f902429
Hauke Stieler <mail@hauke-stieler.de>
2020-09-08 20:09:19 +0200

    React to changes in selected language

--- client/src/app/ui/language-selection/language-selection.component.ts
+++ client/src/app/ui/language-selection/language-selection.component.ts
@@ -9,16 +9,17 @@ import { SelectedLanguageService } from '../../common/selected-language.service'
   styleUrls: ['./language-selection.component.scss']
 })
 export class LanguageSelectionComponent implements OnInit {
-  selectedLanguage: Language;
   languages: Language[] = [];
 
   constructor(private route: ActivatedRoute, private selectedLanguageService: SelectedLanguageService) {
     this.languages = this.selectedLanguageService.getKnownLanguages();
-    this.selectedLanguage = this.selectedLanguageService.getSelectedLanguage();
-    console.log(this.selectedLanguage);
   }
 
   ngOnInit(): void {
+  }
+
+  get selectedLanguage(): Language {
+    return this.selectedLanguageService.getSelectedLanguage();
   }
 
   onLanguageChange(event: any) {
