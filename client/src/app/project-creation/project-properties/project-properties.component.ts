af2f5886890bac623afb6ca9f4d08e0bd35c5a6e
Hauke Stieler <mail@hauke-stieler.de>
2021-01-16 23:48:35 +0100

    Fix #128 error in validation of nested forms

--- client/src/app/project-creation/project-properties/project-properties.component.ts
+++ client/src/app/project-creation/project-properties/project-properties.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { ProjectProperties } from '../project-properties';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-project-properties',
   templateUrl: './project-properties.component.html',
-  styleUrls: ['./project-properties.component.scss']
+  styleUrls: ['./project-properties.component.scss'],
+  viewProviders: [{provide: ControlContainer, useExisting: NgForm}]
 })
 export class ProjectPropertiesComponent {
   @Input() projectProperties: ProjectProperties;
