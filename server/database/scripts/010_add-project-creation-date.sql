9e89aff95655e54d2d623ddfe15a25e7792ebc1c
Hauke Stieler <mail@hauke-stieler.de>
2021-02-13 16:20:16 +0100

    Use timestamp instead of date

--- server/database/scripts/010_add-project-creation-date.sql
+++ server/database/scripts/010_add-project-creation-date.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 
 -- Can be null for old data before adding this column
-ALTER TABLE projects ADD COLUMN creation_date DATE;
+ALTER TABLE projects ADD COLUMN creation_date TIMESTAMP;
 
 INSERT INTO db_versions VALUES('010');
 
