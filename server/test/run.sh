d8fb86d3ecf1a726246efc4ab6017f7a35c54ed9
Hauke Stieler <mail@hauke-stieler.de>
2021-03-09 22:20:20 +0100

    Adjust run.sh and test database dump

--- server/test/run.sh
+++ server/test/run.sh
@@ -16,28 +16,21 @@ echo
 echo "Any key to continue ..."
 read
 
-# Switch to "./" (root folder of repo) where the docker-compose.yml and postgres-data is
-cd ../../
-
-echo "Remove 'postgres-data' folder"
-sudo rm -rf postgres-data
-
 # If container "stm-db" exists, stop and remove it
 if docker container list | grep -q "stm-db"
 then
-	echo "Stop and remove container 'stm-db'"
-	docker stop stm-db
-	docker rm -f stm-db
+	echo "Remove existing database"
+	psql -h localhost -U $STM_DB_USERNAME postgres -tc "DROP DATABASE stm;"
+else
+	cd ../../
+	echo "Start new 'stm-db' container"
+	docker-compose up -d --build stm-db
+	wait
+	cd server/test/
 fi
 
-echo "Start new 'stm-db' container"
-docker-compose up -d --build stm-db
-
-# Wait until container is up
-wait
-
 echo "Initialize new database"
-cd server/database
+cd ../database
 ./init-db.sh
 
 # Switch from "./server/database" into "./server" folder
