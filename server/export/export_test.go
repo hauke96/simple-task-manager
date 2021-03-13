c4b91ecead8af9a62e5cc41d90194b6da427e7cf
Hauke Stieler <mail@hauke-stieler.de>
2021-03-14 00:31:57 +0100

    Add import tests

--- server/export/export_test.go
+++ server/export/export_test.go
@@ -80,3 +80,96 @@ func TestGetProjectExport(t *testing.T) {
 		return nil
 	})
 }
+
+func TestImportProject(t *testing.T) {
+	h.Run(t, func() error {
+		// Arrange
+		task := &TaskExport{
+			Name:             "task 1",
+			ProcessPoints:    33,
+			MaxProcessPoints: 120,
+			Geometry:         "{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[0.00008929616120192039,0.0004811765447811922],[0.00008929616120192039,0.00048118462350998925],[0.00008930976265082209,0.00048118462350998925],[0.00008930976265082209,0.0004811765447811922],[0.00008929616120192039,0.0004811765447811922]]]},\"properties\":null}",
+			AssignedUser:     "345",
+		}
+
+		time := time.Date(2021, 2, 13, 5, 16, 55, 150015000, time.UTC)
+		projectExport := &ProjectExport{
+			Name:         "Test project",
+			Users:        []string{"123", "345"},
+			Owner:        "123",
+			Description:  "foo",
+			CreationDate: &time,
+			Tasks:        []*TaskExport{task},
+		}
+
+		// Act
+		result, err := s.ImportProject(projectExport, "123")
+
+		// Assert
+		if err != nil {
+			return err
+		}
+
+		if result.Name != "Test project" {
+			return errors.New("Project name not matching")
+		}
+		if len(result.Users) != 3 {
+			return errors.New("Number of users not matching")
+		}
+		if *result.CreationDate == time {
+			return errors.New("Project creationDate should not be the original one")
+		}
+		if result.Description != "foo" {
+			return errors.New("Project description not matching")
+		}
+		if result.Owner != "42" {
+			return errors.New("Project description not matching")
+		}
+		if len(result.Tasks) != 1 {
+			return errors.New("Number of tasks not matching")
+		}
+
+		return nil
+	})
+}
+
+func TestImportProjectByDifferentUser(t *testing.T) {
+	h.Run(t, func() error {
+		// Arrange
+		task := &TaskExport{
+			Name:             "task 1",
+			ProcessPoints:    33,
+			MaxProcessPoints: 120,
+			Geometry:         "{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[0.00008929616120192039,0.0004811765447811922],[0.00008929616120192039,0.00048118462350998925],[0.00008930976265082209,0.00048118462350998925],[0.00008930976265082209,0.0004811765447811922],[0.00008929616120192039,0.0004811765447811922]]]},\"properties\":null}",
+			AssignedUser:     "345",
+		}
+
+		projectExport := &ProjectExport{
+			Name:  "Test project",
+			Users: []string{"123", "345"},
+			Owner: "123",
+			Tasks: []*TaskExport{task},
+		}
+
+		requestingUserId := "42"
+
+		// Act
+		result, err := s.ImportProject(projectExport, requestingUserId)
+
+		// Assert
+		if err != nil {
+			return err
+		}
+
+		if len(result.Users) != 3 {
+			return errors.New("Number of users not matching")
+		}
+		if result.Users[0] != requestingUserId &&
+			result.Users[1] != requestingUserId &&
+			result.Users[2] != requestingUserId {
+			return errors.New("Requesting user not member of imported project")
+		}
+
+		return nil
+	})
+}
