c37cd6825da2b5666db5f3687e8b8d8fab5c1e89
Hauke Stieler <mail@hauke-stieler.de>
2021-03-29 16:22:20 +0200

    Test shortcuts in draw component

--- client/src/app/project-creation/drawing-toolbar/drawing-toolbar.component.spec.ts
+++ client/src/app/project-creation/drawing-toolbar/drawing-toolbar.component.spec.ts
@@ -2,16 +2,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DrawingToolbarComponent } from './drawing-toolbar.component';
 import { Subject } from 'rxjs';
+import { ShortcutService } from '../../common/shortcut.service';
 
 describe('DrawingToolbarComponent', () => {
   let component: DrawingToolbarComponent;
   let fixture: ComponentFixture<DrawingToolbarComponent>;
+  let shortcutService: ShortcutService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DrawingToolbarComponent]
     })
       .compileComponents();
+
+    shortcutService = TestBed.inject(ShortcutService);
   });
 
   beforeEach(() => {
@@ -65,5 +69,81 @@ describe('DrawingToolbarComponent', () => {
     component.resetSelection.next();
 
     expect(component.selectedButton).toBeUndefined();
+  });
+
+  it('should deselect selected button', () => {
+    component.selectedButton = component.SELECTION_EDIT;
+    const spy = spyOn(component.buttonEdit, 'emit');
+
+    // @ts-ignore
+    component.onCurrentActiveButton();
+
+    expect(spy).toHaveBeenCalled();
+    expect(component.selectedButton).toBeUndefined();
+  });
+
+  describe('with shortcuts', () => {
+    let shortcutDrawSubject: Subject<void>;
+    let shortcutDeleteSubject: Subject<void>;
+    let shortcutEditSubject: Subject<void>;
+    let shortcutEscapeSubject: Subject<void>;
+
+    beforeEach(() => {
+      shortcutDrawSubject = new Subject<void>();
+      shortcutDeleteSubject = new Subject<void>();
+      shortcutEditSubject = new Subject<void>();
+      shortcutEscapeSubject = new Subject<void>();
+
+      spyOn(shortcutService, 'add')
+        .withArgs('d').and.returnValue(shortcutDrawSubject.asObservable())
+        .withArgs('shift.d').and.returnValue(shortcutDeleteSubject.asObservable())
+        .withArgs('e').and.returnValue(shortcutEditSubject.asObservable())
+        .withArgs('esc').and.returnValue(shortcutEscapeSubject.asObservable());
+
+      fixture = TestBed.createComponent(DrawingToolbarComponent);
+      component = fixture.componentInstance;
+      component.resetSelection = new Subject<void>();
+      fixture.detectChanges();
+    });
+
+    it('should activate draw button on shortcut', () => {
+      component.selectedButton = undefined;
+
+      shortcutDrawSubject.next();
+      expect(component.selectedButton).toEqual(component.SELECTION_DRAW);
+
+      shortcutDrawSubject.next();
+      expect(component.selectedButton).toBeUndefined();
+    });
+
+    it('should activate delete button on shortcut', () => {
+      component.selectedButton = undefined;
+
+      shortcutDeleteSubject.next();
+      expect(component.selectedButton).toEqual(component.SELECTION_DELETE);
+
+      shortcutDeleteSubject.next();
+      expect(component.selectedButton).toBeUndefined();
+    });
+
+    it('should activate edit button on shortcut', () => {
+      component.selectedButton = undefined;
+
+      shortcutEditSubject.next();
+      expect(component.selectedButton).toEqual(component.SELECTION_EDIT);
+
+      shortcutEditSubject.next();
+      expect(component.selectedButton).toBeUndefined();
+    });
+
+    it('should deactivate all buttons on shortcut', () => {
+      component.selectedButton = component.SELECTION_EDIT;
+
+      shortcutEscapeSubject.next();
+      expect(component.selectedButton).toBeUndefined();
+
+      shortcutEscapeSubject.next();
+      expect(component.selectedButton).toBeUndefined();
+    });
   });
 });
