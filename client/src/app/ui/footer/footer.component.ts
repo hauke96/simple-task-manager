b794a3f4d7d408fc40a53e82ee4c4c444a73b2e1
Hauke Stieler <mail@hauke-stieler.de>
2022-03-18 23:33:59 +0100

    Migrate component templates to ngx-translate

--- client/src/app/ui/footer/footer.component.ts
+++ client/src/app/ui/footer/footer.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import packageInfo from '../../../../package.json';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FooterComponent implements OnInit {
   public version: string = packageInfo.version;
