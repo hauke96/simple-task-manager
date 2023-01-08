fd439c74ac2b2fa1204409ad25565814e222705f
Hauke Stieler <mail@hauke-stieler.de>
2023-01-08 16:43:31 +0100

    Use JSON for changeset and notes API #188

--- client/src/app/user/user.service.spec.ts
+++ client/src/app/user/user.service.spec.ts
@@ -36,69 +36,80 @@ const userList = {
   ]
 };
 
-const changesets = `
-<osm version="0.6" generator="OpenStreetMap server" copyright="OpenStreetMap and contributors" attribution="http://www.openstreetmap.org/copyright" license="http://opendatacommons.org/licenses/odbl/1-0/">
-  <changeset id="123" created_at="2020-05-25T10:23:09Z" open="false" comments_count="0" changes_count="7" closed_at="2020-05-25T10:23:09Z" min_lat="0" min_lon="0" max_lat="1" max_lon="1" uid="1" user="foo">
-    <tag k="changesets_count" v="1"/>
-    <tag k="imagery_used" v="Esri World Imagery"/>
-    <tag k="locale" v="de"/>
-    <tag k="host" v="https://www.openstreetmap.org/edit"/>
-    <tag k="created_by" v="simple-task-manager of course ;)"/>
-    <tag k="comment" v="some comment"/>
-  </changeset>
-</osm>
-`;
+const changesets = `{
+  "version": "0.6",
+  "generator": "OpenStreetMap server",
+  "copyright": "OpenStreetMap and contributors",
+  "attribution": "http://www.openstreetmap.org/copyright",
+  "license": "http://opendatacommons.org/licenses/odbl/1-0/",
+  "changesets": [
+    {
+      "id": 123,
+      "uid": 1,
+      "user": "foo",
+      "tags": {
+        "abc": "def",
+      }
+    }
+  ]
+}`;
 
-const comments = `
-<osm version="0.6" generator="OpenStreetMap server" copyright="OpenStreetMap and contributors" attribution="http://www.openstreetmap.org/copyright" license="http://opendatacommons.org/licenses/odbl/1-0/">
-  <note lon="9" lat="53">
-  <id>234</id>
-  <url>
-    https://master.apis.dev.openstreetmap.org/api/0.6/notes/234
-  </url>
-  <comment_url>
-    https://master.apis.dev.openstreetmap.org/api/0.6/notes/234/comment
-  </comment_url>
-  <close_url>
-    https://master.apis.dev.openstreetmap.org/api/0.6/notes/234/close
-  </close_url>
-  <date_created>2020-05-18 19:19:44 UTC</date_created>
-  <status>open</status>
-  <comments>
-    <comment>
-      <date>2020-05-18 19:19:44 UTC</date>
-      <uid>1</uid>
-      <user>foo</user>
-      <user_url>
-        https://master.apis.dev.openstreetmap.org/user/foo
-      </user_url>
-      <action>opened</action>
-      <text>foo</text>
-      <html><p>foo</p></html>
-    </comment>
-  </comments>
-  </note>
-</osm>
-`;
-const invalidComments = `
-<osm version="0.6" generator="OpenStreetMap server" copyright="OpenStreetMap and contributors" attribution="http://www.openstreetmap.org/copyright" license="http://opendatacommons.org/licenses/odbl/1-0/">
-  <note lon="9" lat="53">
-  <comments>
-    <commentoInvalido>
-      <date>2020-05-18 19:19:44 UTC</date>
-      <uid>1</uid>
-      <user>foo</user>
-      <user_url>
-        https://master.apis.dev.openstreetmap.org/user/foo
-      </user_url>
-      <action>opened</action>
-      <text>foo</text>
-      <html><p>foo</p></html>
-    </commentoInvalido>
-  </comments>
-  </note>
-</osm>
-`;
+const comments = `{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.9551,
+          53.546
+        ]
+      },
+      "properties": {
+        "id": 22328,
+        "comments": [
+          {
+            "date": "2020-05-18 19:19:44 UTC",
+            "uid": 1,
+            "user": "foo",
+            "user_url": "https://master.apis.dev.openstreetmap.org/user/hauke-stieler-dev-notes",
+            "action": "opened",
+            "text": "foo"
+          }
+        ]
+      }
+    }
+  ]
+}`;
+const invalidComments = `{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.9551,
+          53.546
+        ]
+      },
+      "invalido-propertido": {
+        "id": 22328,
+        "comments": [
+          {
+            "date": "2020-05-18 19:19:44 UTC",
+            "uid": 1,
+            "user": "foo",
+            "user_url": "https://master.apis.dev.openstreetmap.org/user/hauke-stieler-dev-notes",
+            "action": "opened",
+            "text": "foo"
+          }
+        ]
+      }
+    }
+  ]
+}`;
 
 describe(UserService.name, () => {
   let service: UserService;
@@ -239,7 +250,7 @@ describe(UserService.name, () => {
     );
   });
 
-  it('should return error on invalid comment XML', () => {
+  it('should return error on invalid comment result', () => {
     // @ts-ignore
     httpClient.get = jest.fn().mockImplementation((url: string, options: {}) => {
       if (url.includes('notes')) {
--- client/src/app/user/user.service.ts
+++ client/src/app/user/user.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { User } from './user.material';
+import { GeoJSON } from 'ol/format';
 
 @Injectable({
   providedIn: 'root'
@@ -26,13 +27,23 @@ export class UserService {
 
     // TODO handle case of removed account: Here a comma separated list of users will return a 404 when one UID doesn't exist anymore
     // The users API support JSON
-    return this.http.get(url, {headers: {ContentType: 'application/json'}}).pipe(
+    return this.http.get(url, {headers: {Accept: 'application/json'}}).pipe(
       map(result => {
-        const loadedUsers = this.extractUserFromJsonResult(result);
+        const loadedUsers = this.getUsersFromUserApiResult(result);
         loadedUsers.forEach(u => this.cache.set(u.uid, u));
         return cachedUsers.concat(loadedUsers);
       })
     );
+  }
+
+  private getUsersFromUserApiResult(result: any): User[] {
+    const users = [];
+    for (const u of result.users) {
+      const name = u?.user?.display_name;
+      const uid = u?.user?.id;
+      users.push(new User(name, '' + uid));
+    }
+    return users;
   }
 
   public getUserByName(userName: string): Observable<User> {
@@ -45,16 +56,16 @@ export class UserService {
     const changesetUrl = environment.osm_api_url + '/changesets?display_name=' + userName;
     const notesUrl = environment.osm_api_url + '/notes/search?display_name=' + userName;
 
-    return this.http.get(changesetUrl, {responseType: 'text', headers: {ContentType: 'application/xml'}}).pipe(
+    return this.http.get(changesetUrl, {responseType: 'text', headers: {Accept: 'application/json'}}).pipe(
       map(result => {
-        const userFromXml = this.extractUserFromXmlResult(result, 'changeset', 'user', 'uid');
-        if (!userFromXml || !userFromXml[0]) {
+        const uid = JSON.parse(result)?.changesets[0]?.user;
+        if (uid != null) {
+          const user = new User(userName, uid);
+          this.cache.set(uid, user);
+          return user;
+        } else {
           throw new Error('User \'' + userName + '\' not found in changesets');
         }
-
-        const user = userFromXml[0];
-        this.cache.set(user.uid, user);
-        return user;
       }),
       catchError((e: HttpErrorResponse) => {
         // This error might occur when the user hasn't created a changeset yet. Therefore we don't use the error service here.
@@ -62,74 +73,21 @@ export class UserService {
         console.error(e);
 
         // Second try, this time via the notes API
-        return this.http.get(notesUrl, {responseType: 'text', headers: {ContentType: 'application/xml'}}).pipe(
+        return this.http.get(notesUrl, {responseType: 'text', headers: {Accept: 'application/json'}}).pipe(
           map(result => {
-            const user = this.extractDataFromComment(result, userName);
-            if (!user) {
+            console.log(new GeoJSON().readFeatures(result));
+            const allNoteComments = new GeoJSON().readFeatures(result)?.flatMap(f => f.get('comments'));
+            const uid = allNoteComments.filter(c => c.user === userName)[0]?.uid;
+            if (!uid) {
               throw new Error('User \'' + userName + '\' not found in notes');
             }
-            this.cache.set(user.uid, user);
+            const user = new User(userName, uid);
+            this.cache.set(uid, user);
             return user;
           })
         );
       })
     );
-  }
-
-  // Takes the name of a XML-node (e.g. "user" or "changeset" and finds the according attribute
-  private extractUserFromXmlResult(xmlString: string, nodeQualifier: string, nameQualifier: string, idQualifier: string)
-    : User[] | undefined {
-    if (window.DOMParser) {
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString('' + xmlString, 'application/xml');
-      const userNodes = xmlDoc.getElementsByTagName(nodeQualifier);
-
-      const userNames: User[] = [];
-      for (let i = 0; i < userNodes.length; i++) {
-        // @ts-ignore
-        const name = userNodes[i].attributes.getNamedItem(nameQualifier).value;
-        // @ts-ignore
-        const uid = userNodes[i].attributes.getNamedItem(idQualifier).value;
-        userNames[i] = new User(name, uid);
-      }
-
-      return userNames;
-    }
-
-    return undefined;
-  }
-
-  private extractUserFromJsonResult(result: any): User[] {
-    const users = [];
-    for (const u of result.users) {
-      const name = u?.user?.display_name;
-      const uid = u?.user?.id;
-      users.push(new User(name, '' + uid));
-    }
-    return users;
-  }
-
-  private extractDataFromComment(xmlString: string, userName: string): User | undefined {
-    if (window.DOMParser) {
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString('' + xmlString, 'application/xml');
-      const note = xmlDoc.getElementsByTagName('note')[0];
-      const comments = note.getElementsByTagName('comments');
-      const commentNodes = comments[0].getElementsByTagName('comment');
-
-      // tslint:disable-next-line:prefer-for-of
-      for (let i = 0; i < commentNodes.length; i++) {
-        // Check whether the user of the comment is the user we search for
-        const name = (commentNodes[i].getElementsByTagName('user'))[0].textContent;
-        if (name === userName) {
-          const uid = commentNodes[i].getElementsByTagName('uid')[0].textContent;
-          if (!!uid) {
-            return new User(name, uid);
-          }
-        }
-      }
-    }
-    return undefined;
   }
 
   public getFromCacheById(userIds: string[]): [User[], string[]] {
