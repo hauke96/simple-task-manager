b46704f8491524ff1f2679165f1ad5b6160973d6
Hauke Stieler <mail@hauke-stieler.de>
2024-03-24 22:05:39 +0100

    Fix wrong JSON name of author-ID

--- server/comment/entity.go
+++ server/comment/entity.go
@@ -5,6 +5,6 @@ import "time"
 type Comment struct {
 	Id           string     `json:"id"`           // The ID of the task.
 	Text         string     `json:"text"`         // The name of the task. If the properties of the geometry feature contain the field "name", this field is used here. If no name has been set, this field will be empty.
-	AuthorId     string     `json:"assignedUser"` // The user-ID of the user who is currently assigned to this task. Will never be NULL but might be empty.
+	AuthorId     string     `json:"authorId"`     // The user-ID of the user who is currently assigned to this task. Will never be NULL but might be empty.
 	CreationDate *time.Time `json:"creationDate"` // The time this comment was created at.
 }
