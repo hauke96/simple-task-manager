ab6e11559ba6469d070274730030f3c90fd8cd5d
Hauke Stieler <mail@hauke-stieler.de>
2024-06-14 11:48:25 +0200

    Set Version to 1.6.0

--- server/docs/docs.go
+++ server/docs/docs.go
@@ -764,7 +764,7 @@ type swaggerInfo struct {
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
-	Version:     "1.5.2",
+	Version:     "1.6.0",
 	Host:        "",
 	BasePath:    "",
 	Schemes:     []string{},
--- server/main.go
+++ server/main.go
@@ -34,7 +34,7 @@ func configureLogging() {
 }
 
 // @title SimpleTaskManager Server
-// @version 1.5.2
+// @version 1.6.0
 // @description This is the SimpleTaskManager (STM) Server. See the GitHub repo '/doc/api/' for further details on authentication, websockets and changelogs.
 
 // @contact.name STM issue tracker
