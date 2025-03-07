3269f42a33d544eceb07af4068a497166a486c61
Hauke Stieler <mail@hauke-stieler.de>
2025-03-07 09:56:47 +0100

    Set version to 1.7.0-SNAPSHOT.2

--- server/docs/docs.go
+++ server/docs/docs.go
@@ -764,7 +764,7 @@ type swaggerInfo struct {
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
-	Version:     "1.6.1",
+	Version:     "1.7.0-SNAPSHOT.2",
 	Host:        "",
 	BasePath:    "",
 	Schemes:     []string{},
