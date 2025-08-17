0cb462434dbe698d673c4b62fb3c4ece8c062e84
Hauke Stieler <mail@hauke-stieler.de>
2025-08-17 09:50:18 +0200

    Set version to 1.7.0

--- server/docs/docs.go
+++ server/docs/docs.go
@@ -764,7 +764,7 @@ type swaggerInfo struct {
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
-	Version:     "1.7.0-SNAPSHOT.2",
+	Version:     "1.7.0",
 	Host:        "",
 	BasePath:    "",
 	Schemes:     []string{},
--- server/util/util.go
+++ server/util/util.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	VERSION = "1.7.0-SNAPSHOT.2"
+	VERSION = "1.7.0"
 )
 
 func GetParam(param string, r *http.Request) (string, error) {
